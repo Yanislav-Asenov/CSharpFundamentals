7b2b7f466e67e3658b22556bace770f5fd21eb12
Yanislav <yanislav.asenov@gmail.com>
2017-05-29 20:34:59 +0300

    Add part 1 from the bashsoft lab

---
+++ BashSoft/BashSoft/Launcher.cs
@@ -0,0 +1,10 @@
+﻿namespace BashSoft
+{
+    public class Launcher
+    {
+        public static void Main()
+        {
+            DirectoryInfo.TraverseDirectory(@"C:\Users\Yanislav Asenov\Desktop\Github Repos\CSharpFundamentals\BashSoft\BashSoft");
+        }
+    }
+}
