7a970285cf227215776fbdfcb82499dd66e570f9
Yanislav <yanislav.asenov@gmail.com>
2017-07-02 12:53:37 +0300

    Add encapsulation lab

---
+++ OOPBasics/EncapsulationLab/People/Program.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+        var lines = int.Parse(Console.ReadLine());
+        var persons = new List<Person>();
+        for (int i = 0; i < lines; i++)
+        {
+            try
+            {
+                var cmdArgs = Console.ReadLine().Split();
+                var person = new Person(cmdArgs[0],
+                                        cmdArgs[1],
+                                        int.Parse(cmdArgs[2]),
+                                        double.Parse(cmdArgs[3]));
+
+                persons.Add(person);
+            }
+            catch(ArgumentException argumentExcepton)
+            {
+                Console.WriteLine(argumentExcepton.Message);
+            }
+        }
+        var bonus = double.Parse(Console.ReadLine());
+        persons.ForEach(p => 
+        {
+            p.IncreaseSalary(bonus);
+            Console.WriteLine(p);
+        });
+    }
+
+}
+
+
+public class Person
+{
+    private string firstName;
+    private string lastName;
+    private int age;
+    private double salary;
+
+    public string FirstName
+    {
+        get
+        {
+            return this.firstName;
+        }
+        set
+        {
+            if (value.Length < 3)
+            {
+                throw new ArgumentException("First name cannot be less than 3 symbols");
+            }
+
+            this.firstName = value;
+        }
+    }
+
+    private string LastName
+    {
+        get
+        {
+            return this.lastName;
+        }
+        set
+        {
+            if (value.Length < 3)
+            {
+                throw new ArgumentException("Last name cannot be less than 3 symbols");
+            }
+
+            this.lastName = value;
+        }
+    }
+
+    public int Age
+    {
+        get
+        {
+            return this.age;
+        }
+        set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Age cannot be zero or negative integer");
+            }
+
+            this.age = value;
+        }
+    }
+
+    public double Salary
+    {
+        get
+        {
+            return this.salary;
+        }
+        set
+        {
+            if (value < 460.0)
+            {
+                throw new ArgumentException("Salary cannot be less than 460 leva");
+            }
+
+            this.salary = value;
+        }
+    }
+
+    public Person(string firstName, string lastName, int age, double salary)
+    {
+        this.FirstName = firstName;
+        this.LastName = lastName;
+        this.Age = age;
+        this.Salary = salary;
+    }
+
+    public override string ToString()
+    {
+        return $"{this.firstName} {this.lastName} get {this.salary:F2} leva";
+    }
+
+    public void IncreaseSalary(double bonus)
+    {
+        if (this.age > 30)
+        {
+            this.salary += this.salary * bonus / 100;
+        }
+        else
+        {
+            this.salary += this.salary * bonus / 200;
+        }
+    }
+}
+
+public class Team
+{
+    private string name;
+    private List<Person> firstTeam;
+    private List<Person> reserveTeam;
+
+    public IReadOnlyCollection<Person> FirstTeam
+    {
+        get
+        {
+            return this.firstTeam.AsReadOnly();
+        }
+    }
+
+    public IReadOnlyCollection<Person> ReserveTeam
+    {
+        get
+        {
+            return this.reserveTeam.AsReadOnly();
+        }
+    }
+
+    public Team(string name)
+    {
+        this.name = name;
+        this.firstTeam = new List<Person>();
+        this.reserveTeam = new List<Person>();
+    }
+
+    public void AddPlayer(Person player)
+    {
+        if (player.Age < 40)
+        {
+            this.firstTeam.Add(player);
+        }
+        else
+        {
+            this.reserveTeam.Add(player);
+        }
+    }
+}
