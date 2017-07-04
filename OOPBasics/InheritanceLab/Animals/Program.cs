aa0828bd58d0f2d60960e4b2c8ba68c26bef8afc
Yanislav <yanislav.asenov@gmail.com>
2017-07-04 20:58:42 +0300

    Add Inheritance lab exercises

---
+++ OOPBasics/InheritanceLab/Animals/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Puppy dog = new Puppy();
+        dog.Eat();
+        dog.Bark();
+
+        Cat cat = new Cat();
+        cat.Eat();
+        cat.Meow();
+    }
+}
+
