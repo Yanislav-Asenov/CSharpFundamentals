b7c9c1fece814469a2327f9a85b021a72b13bdea
Yanislav <yanislav.asenov@gmail.com>
2017-07-19 19:01:17 +0300

    Add interfaces lab exercises

---
+++ OOPAdvanced/InterfacesLab/Cars/Models/Seat.cs
@@ -0,0 +1,7 @@
+﻿public class Seat : Car
+{
+    public Seat(string mode, string color) 
+        : base(mode, color)
+    {
+    }
+}
---
+++ OOPAdvanced/InterfacesLab/Cars/Models/Tesla.cs
@@ -0,0 +1,21 @@
+﻿public class Tesla : Car, IElectricCar
+{
+    private int battery;
+
+    public Tesla(string mode, string color, int battery) 
+        : base(mode, color)
+    {
+        this.Battery = battery;
+    }
+
+    public int Battery
+    {
+        get => this.battery;
+        set => this.battery = value;
+    }
+
+    public override string ToString()
+    {
+        return $"{base.ToString()} with {this.Battery} Batteries";
+    }
+}
