3dcf070ef851645b0478753468b5c1ee57215676
Yanislav <yanislav.asenov@gmail.com>
2017-07-21 20:16:00 +0300

    Add tuple exercises

--- OOPAdvanced/GenericsExercises/CustomTuple/Models/Tuple.cs
+++ OOPAdvanced/GenericsExercises/CustomTuple/Models/Tuple.cs
@@ -1,16 +1,36 @@
-﻿public class Tuple<Item1, Item2>
+﻿public class Threeuple<T1, T2, T3>
 {
-    private Item1 item1;
-    private Item2 item2;
+    private T1 item1;
+    private T2 item2;
+    private T3 item3;
 
-    public Tuple(Item1 item1, Item2 item2)
+    public Threeuple(T1 item1, T2 item2, T3 item3)
     {
         this.item1 = item1;
         this.item2 = item2;
+        this.item3 = item3;
+    }
+
+    public T1 Item1
+    {
+        get => this.item1;
+        set => this.item1 = value;
+    }
+
+    public T2 Item2
+    {
+        get => this.item2;
+        set => this.item2 = value;
+    }
+
+    public T3 Item3
+    {
+        get => this.item3;
+        set => this.item3 = value;
     }
 
     public override string ToString()
     {
-        return $"{this.item1} -> {this.item2}";
+        return $"{this.item1} -> {this.item2} -> {this.item3}";
     }
 }
--- OOPAdvanced/GenericsExercises/CustomTuple/Startup.cs
+++ OOPAdvanced/GenericsExercises/CustomTuple/Startup.cs
@@ -4,23 +4,14 @@ public class Startup
 {
     public static void Main()
     {
-        string[] tuple1Args = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-        Tuple<string, string> tuple1 = new Tuple<string, string>(tuple1Args[0] + " " + tuple1Args[1], tuple1Args[2]);
+        string[] tuple1Args = Console.ReadLine().Split();
+        Threeuple<string, string, string> tuple1 = new Threeuple<string, string, string>(tuple1Args[0] + " " + tuple1Args[1], tuple1Args[2], tuple1Args[3]);
 
-        string[] tuple2Args = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-        Tuple<string, int> tuple2;
+        string[] tuple2Args = Console.ReadLine().Split();
+        Threeuple<string, int, bool> tuple2 = new Threeuple<string, int, bool>(tuple2Args[0], int.Parse(tuple2Args[1]), tuple2Args[2] == "drunk" ? true: false);
 
-        if (tuple2Args.Length < 2)
-        {
-            tuple2 = new Tuple<string, int>(tuple2Args[0], 0);
-        }
-        else
-        {
-            tuple2 = new Tuple<string, int>(tuple2Args[0], int.Parse(tuple2Args[1]));
-        }
-
-        string[] tuple3Args = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-        Tuple<string, double> tuple3 = new Tuple<string, double>(tuple3Args[0], double.Parse(tuple3Args[1]));
+        string[] tuple3Args = Console.ReadLine().Split();
+        Threeuple<string, double, string> tuple3 = new Threeuple<string, double, string>(tuple3Args[0], double.Parse(tuple3Args[1]), tuple3Args[2]);
 
         Console.WriteLine(tuple1);
         Console.WriteLine(tuple2);
