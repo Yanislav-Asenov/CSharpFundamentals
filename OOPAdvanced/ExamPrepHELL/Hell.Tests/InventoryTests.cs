9986aaa1d8c9dc7569c099e93ffafb3068465d5f
Yanislav <yanislav.asenov@gmail.com>
2017-08-19 15:10:46 +0300

    Add Unit Tests Solved for 100/100

--- OOPAdvanced/ExamPrepHELL/Hell.Tests/InventoryTests.cs
+++ OOPAdvanced/ExamPrepHELL/Hell.Tests/InventoryTests.cs
@@ -1,69 +1,159 @@
 ﻿using NUnit.Framework;
+using System.Collections.Generic;
 
 [TestFixture]
 public class InventoryTests
 {
-    [Test]
-    public void AddCommonItemShouldIncreaseBonusStats()
+    private IInventory inventory;
+
+    [SetUp]
+    public void SetUp()
     {
-        // Assert
-        IInventory inventory = new HeroInventory();
-        IItem item = new CommonItem("Test Item 1", 15, 15, 15, 15, 15);
-
-        // Act
-        inventory.AddCommonItem(item);
-
-        // Assert
-        Assert.AreEqual(15, inventory.TotalStrengthBonus);
-        Assert.AreEqual(15, inventory.TotalAgilityBonus);
-        Assert.AreEqual(15, inventory.TotalIntelligenceBonus);
-        Assert.AreEqual(15, inventory.TotalHitPointsBonus);
-        Assert.AreEqual(15, inventory.TotalDamageBonus);
+        this.inventory = new HeroInventory();
     }
 
     [Test]
-    public void AddMultipleCommonItemsShouldIncreaseTotalBonusStats()
+    public void AddCommonItemShouldIncreaseTotalStrengthBonus()
     {
+        // Arrange
+        IItem item = new CommonItem("Test Item 1", 15, 0, 0, 0, 0);
+
+        // Act
+        this.inventory.AddCommonItem(item);
+
         // Assert
-        IInventory inventory = new HeroInventory();
+        Assert.AreEqual(15, this.inventory.TotalStrengthBonus);
+    }
+
+    [Test]
+    public void AddCommonItemShouldIncreaseTotalAgilityBonus()
+    {
+        // Arrange
+        IItem item = new CommonItem("Test Item 1", 0, 15, 0, 0, 0);
+
+        // Act
+        this.inventory.AddCommonItem(item);
+
+        // Assert
+        Assert.AreEqual(15, this.inventory.TotalAgilityBonus);
+    }
+
+    [Test]
+    public void AddCommonItemShouldIncreaseTotalIntelligenceBonus()
+    {
+        // Arrange
+        IItem item = new CommonItem("Test Item 1", 0, 0, 15, 0, 0);
+
+        // Act
+        this.inventory.AddCommonItem(item);
+
+        // Assert
+        Assert.AreEqual(15, this.inventory.TotalIntelligenceBonus);
+    }
+
+    [Test]
+    public void AddCommonItemShouldIncreaseTotalHitPointsBonus()
+    {
+        // Arrange
+        IItem item = new CommonItem("Test Item 1", 0, 0, 0, 15, 0);
+
+        // Act
+        this.inventory.AddCommonItem(item);
+
+        // Assert
+        Assert.AreEqual(15, this.inventory.TotalHitPointsBonus);
+    }
+
+    [Test]
+    public void AddCommonItemShouldIncreaseTotalDamageBonus()
+    {
+        // Arrange
+        IItem item = new CommonItem("Test Item 1", 0, 0, 0, 0, 15);
+
+        // Act
+        this.inventory.AddCommonItem(item);
+
+        // Assert
+        Assert.AreEqual(15, this.inventory.TotalDamageBonus);
+    }
+
+    [Test]
+    public void AddMultipleCommonItemsShouldIncreaseTotalStatBonuses()
+    {
+        // Arrange
         IItem item1 = new CommonItem("Test Item 1", 15, 15, 15, 15, 15);
         IItem item2 = new CommonItem("Test Item 2", 15, 15, 15, 15, 15);
         IItem item3 = new CommonItem("Test Item 3", 30, 30, 30, 30, 30);
 
         // Act
-        inventory.AddCommonItem(item1);
-        inventory.AddCommonItem(item2);
-        inventory.AddCommonItem(item3);
+        this.inventory.AddCommonItem(item1);
+        this.inventory.AddCommonItem(item2);
+        this.inventory.AddCommonItem(item3);
 
         // Assert
-        Assert.AreEqual(60, inventory.TotalStrengthBonus);
-        Assert.AreEqual(60, inventory.TotalAgilityBonus);
-        Assert.AreEqual(60, inventory.TotalIntelligenceBonus);
-        Assert.AreEqual(60, inventory.TotalHitPointsBonus);
-        Assert.AreEqual(60, inventory.TotalDamageBonus);
+        Assert.AreEqual(60, this.inventory.TotalStrengthBonus);
+        Assert.AreEqual(60, this.inventory.TotalAgilityBonus);
+        Assert.AreEqual(60, this.inventory.TotalIntelligenceBonus);
+        Assert.AreEqual(60, this.inventory.TotalHitPointsBonus);
+        Assert.AreEqual(60, this.inventory.TotalDamageBonus);
     }
 
     [Test]
-    public void AddMultipleItemsWithTotalValueLargerThanIntegerShouldWork()
+    public void AddRecipeWhenRequiredItemsAreAvailableShouldIncreaseTotalStatBonuses()
     {
-        // Assert
-        IInventory inventory = new HeroInventory();
-        IItem item1 = new CommonItem("Test Item 1", int.MaxValue, int.MaxValue, int.MaxValue, int.MaxValue, int.MaxValue);
-        IItem item2 = new CommonItem("Test Item 2", int.MaxValue, int.MaxValue, int.MaxValue, int.MaxValue, int.MaxValue);
+        // Arrange
+        IItem item = new CommonItem("Test Item 1", 0, 0, 0, 0, 0);
+        IRecipe recipe = new RecipeItem("Test Recipe 1", 15, 15, 15, 15, 15, new List<string>() { "Test Item 1" });
 
         // Act
-        inventory.AddCommonItem(item1);
-        inventory.AddCommonItem(item2);
+        this.inventory.AddCommonItem(item);
+        this.inventory.AddRecipeItem(recipe);
 
         // Assert
-        long expectedResult = 0;
-        expectedResult += int.MaxValue;
-        expectedResult += int.MaxValue;
+        Assert.AreEqual(recipe.StrengthBonus, this.inventory.TotalStrengthBonus);
+        Assert.AreEqual(recipe.StrengthBonus, this.inventory.TotalAgilityBonus);
+        Assert.AreEqual(recipe.StrengthBonus, this.inventory.TotalIntelligenceBonus);
+        Assert.AreEqual(recipe.StrengthBonus, this.inventory.TotalHitPointsBonus);
+        Assert.AreEqual(recipe.StrengthBonus, this.inventory.TotalDamageBonus);
+    }
 
-        Assert.AreEqual(expectedResult, inventory.TotalStrengthBonus);
-        Assert.AreEqual(expectedResult, inventory.TotalAgilityBonus);
-        Assert.AreEqual(expectedResult, inventory.TotalIntelligenceBonus);
-        Assert.AreEqual(expectedResult, inventory.TotalHitPointsBonus);
-        Assert.AreEqual(expectedResult, inventory.TotalDamageBonus);
+    [Test]
+    public void AddRecipeWhenRequiredItemsAreNotAvailableShouldNotIncreaseTotalStatBonuses()
+    {
+        // Arrange
+        IItem item = new CommonItem("Test Item 1", 0, 0, 0, 0, 0);
+        IRecipe recipe = new RecipeItem("Test Recipe 1", 15, 15, 15, 15, 15, new List<string>() { "Test Item 2" });
+
+        // Act
+        this.inventory.AddCommonItem(item);
+        this.inventory.AddRecipeItem(recipe);
+
+        // Assert
+        Assert.AreEqual(0, this.inventory.TotalStrengthBonus);
+        Assert.AreEqual(0, this.inventory.TotalAgilityBonus);
+        Assert.AreEqual(0, this.inventory.TotalIntelligenceBonus);
+        Assert.AreEqual(0, this.inventory.TotalHitPointsBonus);
+        Assert.AreEqual(0, this.inventory.TotalDamageBonus);
+    }
+
+    [Test]
+    public void AddCommonItemWhichIsRequiredForRecipeShouldCreateRecipeAndIncreaseTotalStatBonuses()
+    {
+        // Arrange
+        IItem item1 = new CommonItem("Test Item 1", 0, 0, 0, 0, 0);
+        IItem item2 = new CommonItem("Test Item 2", 0, 0, 0, 0, 0);
+        IRecipe recipe = new RecipeItem("Test Recipe 1", 30, 30, 30, 30, 30, new List<string>() { "Test Item 1", "Test Item 2" });
+
+        // Act
+        this.inventory.AddRecipeItem(recipe);
+        this.inventory.AddCommonItem(item1);
+        this.inventory.AddCommonItem(item2);
+
+        // Assert
+        Assert.AreEqual(30, this.inventory.TotalStrengthBonus);
+        Assert.AreEqual(30, this.inventory.TotalAgilityBonus);
+        Assert.AreEqual(30, this.inventory.TotalIntelligenceBonus);
+        Assert.AreEqual(30, this.inventory.TotalHitPointsBonus);
+        Assert.AreEqual(30, this.inventory.TotalDamageBonus);
     }
 }
